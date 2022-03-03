f8afa9c738742b4ff469390166bf3f815f83837f
Kadargo63 <robertisaacs87@yahoo.com>
2022-03-02 21:42:03 -0600

    sum array practice

--- 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array5.js
+++ 04-Web-APIs/03-Algorithms/03-sum-array/Unsolved/sum-array5.js
@@ -2,7 +2,7 @@ var sumArray = function(arr) {
     var result = 0;
 
     for (var i = 0; i < arr.length; i++) {
-        var currentNumber = arr{i};
+        var currentNumber = arr[i];
         result += currentNumber;
     }
 
