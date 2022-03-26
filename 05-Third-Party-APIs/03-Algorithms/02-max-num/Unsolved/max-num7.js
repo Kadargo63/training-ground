7fcafa9686e119b5bbf3fd03762fca6dbc80b57e
Kadargo63 <robertisaacs87@yahoo.com>
2022-03-26 11:44:19 -0500

    practice

--- 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num7.js
+++ 05-Third-Party-APIs/03-Algorithms/02-max-num/Unsolved/max-num7.js
@@ -1,7 +1,7 @@
  var maxNum = function(arr) {
      var max = arr[0];
 
-     for (var i = 1 < arr.length; i++) {
+     for (var i = 1; i < arr.length; i++) {
          var currentNum = arr[i];
 
          if (currentNum > max) {
