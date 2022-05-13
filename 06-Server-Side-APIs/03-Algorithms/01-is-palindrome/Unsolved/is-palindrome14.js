8ed15cd0850865f81eb3c208a4eefdd9b1a687cb
Kadargo63 <robertisaacs87@yahoo.com>
2022-05-13 17:06:38 -0500

    practice

---
+++ 06-Server-Side-APIs/03-Algorithms/01-is-palindrome/Unsolved/is-palindrome14.js
@@ -0,0 +1,13 @@
+var isPalindrome = function(str) {
+    var reversedString = str
+        .split("")
+        .reverse()
+        .join("");
+
+    if (reversedString === str) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
